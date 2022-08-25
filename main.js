0cfc9aae4943fdf36e05391222220d3538ff481f
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-08-26 02:13:47 +0600

    Project completed about 80%

---
+++ main.js
@@ -0,0 +1,46 @@
+
+
+
+const getPrice = () => {
+    const priceElement = document.getElementsByClassName("price");
+    for(const price of priceElement) {
+        const priceText = price.innerText;
+        console.log(priceText);
+    }
+}
+
+const buttons = document.getElementsByClassName("card-btn");
+for( const button of buttons) {
+    button.addEventListener("click", () => {
+        const productName = button.previousSibling.previousSibling.previousSibling.previousSibling.innerText;
+        const productPrice = button.previousSibling.previousSibling.innerText;
+        const table = document.getElementById("table");
+        const tr = document.createElement("tr");
+        const td1 = document.createElement("td");
+        td1.innerText = productName;
+        const td2 = document.createElement("td");
+        td2.innerText = productPrice;
+
+        // console.log(td2.innerText);
+        
+        // add the td tags to the tr tag
+        tr.appendChild(td1)
+        tr.appendChild(td2)
+
+        // add the tr tag to the table tag
+        table.appendChild(tr);
+
+        const productPriceSpan = button.previousSibling.previousSibling;
+        const getSpan = productPriceSpan.childNodes[1].innerText;
+        const priceValue = parseInt(getSpan);
+
+        // get total price element
+        const totalPriceElement = document.getElementById("total-price");
+        const priceString = totalPriceElement.innerText;
+        const totalPrice = parseInt(priceString);
+
+        const currentPrice = totalPrice + priceValue;
+        totalPriceElement.innerText = currentPrice;
+    })
+}
+// console.log(buttons, cards);
